894724660908d198c9548ef6a85a3db927744349
danpaul <danbreczinski@gmail.com>
2013-07-17 23:11:06 +0100

    added csv parsing

--- csv.js
+++ csv.js
@@ -1,5 +1,45 @@
 //npm install to insall packages
 
+//from: http://stackoverflow.com/questions/10425287/convert-string-to-camelcase-with-regular-expression
+function camelCase(input) { 
+     return input
+		.toLowerCase()
+		.replace(/^\s+|\s+$/g,'')
+		.replace(/\s(.)/g, function(match, group1) {
+			return group1.toUpperCase();
+		});
+}
+
+function typeConvert(conversionObject, key, value){
+	if(conversionObject.hasOwnProperty(key)){
+		switch(conversionObject[key]){
+			case "int":
+				console.log("int");
+				console.log(parseInt(value));
+				return parseInt(value);
+		}
+	}
+	return value;
+}
+
+var HEADER_CONVERSION_TYPES = 
+{
+	"borough": "int",
+	"taxClassAtPresent": "int",
+	"block": "int",
+	"lot": "int",
+	"residentialUnits": "int",
+	"commercialUnits": "int",
+	"toalUnits": "int",
+	"land": "int",
+	"squreFeet": "int",
+	"grossSquareFeet": "int",
+	"yearBuilt": "date",
+	"taxClass": "int",
+	"salePrice": "float",
+	"saleDate": "date"
+};
+
 var CSV_FILE = "./short_summary.csv";
 
 var THUNDER_DB_URI = "mongodb://localhost:27017/exampleDb";
@@ -19,16 +59,21 @@ MongoClient.connect(THUNDER_DB_URI, function(err, db) {
 	.transform(function(row, index){
 	if(index === 0){
 			arrayLength = row.length;
-			header = row;
+			header = new Array(arrayLength);
+			for(var i = 0; i < arrayLength; i++){
+				header[i] = camelCase(row[i]);
+			}
+//console.log(header);
 		}else{
-console.log(row);
 			var newRecord = {};
 			for(var i = 0; i < arrayLength; i++){
-				newRecord[header[i]] = row[i];
+				newRecord[header[i]] = typeConvert(HEADER_CONVERSION_TYPES, header[i], row[i]);
+				//newRecord[header[i]] = row[i];
 			}
-			//console.log(newRecord);
+			console.log(newRecord);
 		}
 	})
 	.on('end', function() {console.log("done")});
-
 });
+
+//console.log(camelCase("  Foo bAr"));
