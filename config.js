58002f533eccd5adcb7340b3cc9d4978bcf40b7a
danpaul <pt2323@gmail.com>
2013-07-20 19:58:53 -0400

    updating config

--- config.js
+++ config.js
@@ -1,5 +1,72 @@
 var config = {};
 
-config.dbURI = "mongodb://localhost:27017/thunder";
+config.dbURI = 'mongodb://localhost:27017/thunder';
+
+//------------------------------------------------------------------------------
+// csv.js specific
+//------------------------------------------------------------------------------
+
+config.csvFile = __dirname + '/data/short_summary.csv';
+
+//this will also be the collection name for the base records
+config.salseRecordModelName = 'salesRecord';
+
+config.headerConversionTypes = 
+{
+  borough: "int",
+  taxClassAtPresent: "int",
+  block: "int",
+  lot: "int",
+  residentialUnits: "int",
+  commercialUnits: "int",
+  totalUnits: "int",
+  landSquareFeet: "int",
+  squreFeet: "int",
+  grossSquareFeet: "int",
+  salePrice: "int",
+  yearBuilt: "int",
+  saleDate: "date"
+};
+
+config.salesRecordSchemaObject =
+{
+//String
+  neighborhood: String,
+  buildingClassCategory: String,
+  easement: String,
+  buildingClassAtPresent: String,
+  address: String,
+  apartmentNumber: String,
+  zipCode: String,
+  taxClassAtTimeOfSale: String,
+  buildingClassAtTimeOfSale: String,
+//Number
+  borough: Number,
+  taxClassAtPresent: Number,
+  block: Number,
+  lot: Number,
+  residentialUnits: Number,
+  commercialUnits: Number,
+  totalUnits: Number,
+  landSquareFeet: Number,
+  squreFeet: Number,
+  grossSquareFeet: Number,
+  salePrice: Number,
+  yearBuilt: Number,
+//Date
+  saleDate: Date
+}
+
+config.matchQueryBuilder = function(mongooseModel)
+{
+	return
+	({
+		'address': mongooseModel.address,
+    	'apartmentNumber': mongooseModel.apartmentNumber,
+    	'saleDate': mongooseModel.saleDate
+    });
+}
+
+
 
 module.exports = config;
