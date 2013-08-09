26dea6a56f83aeea12b837c31b199aaa64de641a
unknown <pt2323@gmail.com>
2013-08-09 16:08:31 +0100

    before change monthly neighborhood summaries

--- scripts/test.js
+++ scripts/test.js
@@ -13,37 +13,6 @@ var monthlyBorough = require(config.monthlyBoroughSummaryModelFile);
 var files = fs.readdirSync(config.dataDirectory);
 var TIMEOUT_DELAY = 10000;
 
-async.forEachSeries(files, function(file, callback)
-{
-	function buildFromFile(file, callback)
-	{
-		salesRecord.buildCollection_2(config.dataDirectory + '/' + file, function(){});	
-		var buildInfo = {};
-		buildInfo.recordCount = 0;
-		buildInfo.firstCheck = true;
+meta.buildNeighborhoodList(function(){console.log('done')});
 
-		var buildCheck = function(){
-			salesRecord.model.count({}, function(err, record)
-			{
-				if(err){console.log(err)
-				}else{
-					if(buildInfo.recordCount === record)
-					{
-console.log('salesrecord collection built');
-						callback();
-					}else{
-						buildInfo.recordCount = record;
-console.log(buildInfo.recordCount);
-						takeTimeout();
-					}
-				}
-			});		
-		}
-		function takeTimeout()
-		{
-			setTimeout(buildCheck, TIMEOUT_DELAY);
-		}
-		takeTimeout();
-	}//);//, function(){callback();})
-	buildFromFile(file, callback);
-},function(){console.log('done')});
+meta.model.find({key: config.key.neighborhood}, function(err, record){console.log(record)});
